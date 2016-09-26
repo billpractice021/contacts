35e0b5dca4db79a92332eb3a3e9028e12d8e1db9
Bill Malone <bmalone021@gmail.com>
2016-09-26 23:56:01 +0000

    Updated

--- public/src/app.js
+++ public/src/app.js
@@ -1,11 +1,17 @@
-angular.module('ContactsApp', ['ngRoute']) 
-    .config(function ($routeProvider, $locationProvider) {
-        $routeProvider
-            .when('/contacts', {
-                controller: 'ListController',
-                templateUrl: 'views/list.html'
+angular.module('ContactsApp', ['ngRoute', 'ui.router', 'ngResource']) 
+    .config(function ($stateProvider, $routeProvider, $locationProvider) {
+        // $routeProvider
+        //     .when('/contacts', {
+        //         controller: 'ListController',
+        //         templateUrl: '/views/list.html'
+        //     });
+        $stateProvider
+            .state('contacts', {
+                url: '/contacts',
+                templateUrl: '/views/list.html'
             });
+            
         
-        $locationProvider.html5Mode(true);
+        
     });
     
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require("express"),
+    app     = express('./api'), 
     app     = express(); 
     
 app
     .use(express.static('./public'))
+    .use('/api', api)
+    .listen(process.env.PORT);
     
-    .listen(process.env.PORT);  
