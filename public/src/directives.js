92de173cfebc5c0bc412ba476be79b3f6528b962
Bill Malone <bmalone021@gmail.com>
2016-09-27 04:24:30 +0000

    updated

--- public/src/directives.js
+++ public/src/directives.js
@@ -12,3 +12,41 @@ angular.module('ContactsApp')
         tel: ['Phone Number', 'should be a phone number'],
         color: ['Color', 'should be a color']
     })
+    .directive('formField', function ($timeout, FieldTypes) {
+        return {
+            restrict: 'EA',
+            templateUrl: 'views/form-field.html',
+            replace: true,
+            scope: {
+                record: '=',
+                field: '@',
+                live: '@',
+                required: '@'
+            },
+            link: function ($scope, element, attr) {
+                $scope.$on('record:invalid', function () {
+                    $scope[$scope.field].$setDirty();
+                });
+
+                $scope.types = FieldTypes;
+
+                $scope.remove = function (field) {
+                    delete $scope.record[field];
+                    $scope.blurUpdate();
+                };
+                
+                $scope.blurUpdate = function () {
+                    if ($scope.live !== 'false') {
+                        $scope.record.$update(function (updatedRecord) {
+                            $scope.record = updatedRecord;
+                        });
+                    }
+                };
+                var saveTimeout;
+                $scope.update = function () {
+                    $timeout.cancel(saveTimeout);
+                    saveTimeout = $timeout($scope.blurUpdate, 1000);
+                };
+            }
+        };
+    })
